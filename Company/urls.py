c40ba1c2d39a196a9550890310c2634e90f73bd9
Efrem Yohanis <102489707+efremjon@users.noreply.github.com>
2022-05-02 17:02:27 -0700

    admin profile made change

--- Company/urls.py
+++ Company/urls.py
@@ -3,7 +3,15 @@ from .import views
 urlpatterns = [
     path('',views.Admin_dashboard,name="admin-dashbord"),
     path('add-agent/',views.add_agent,name='add-agent'),
-    path('users-profile/',views.user_profile,name='users-profile'),
+
+    
+    path('show_profile/',views.show_profile,name='show_profile'),
+    path('edit_profile/',views.edit_profile,name='edit_profile'),
+    path('change_password/',views.change_password_m,name='change_password'),
+    path('change_profile_pic/',views.change_profile_pic,name='change_profile_pic'),
+    path('delete_profile_pic/',views.delete_profile_pic,name='delete_profile_pic'),
+
+
     path('add-staff/',views.add_staff,name='add-staff'),
     path('remove-staff/',views.remove_staff,name='remove-staff'),
     path('view-agent-orders/',views.view_agent_orders,name='view-agent-orders'),
--- Company/views.py
+++ Company/views.py
@@ -1,8 +1,11 @@
 from multiprocessing.dummy import JoinableQueue
 from django.contrib import messages
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 from django.shortcuts import render,redirect,reverse
 from django.http import HttpResponse
 from django.contrib.auth.models import User,Group
+from django.contrib.auth.forms import PasswordChangeForm
 from .models import *
 
 # Create your views here.
@@ -56,40 +59,109 @@ def add_agent(request):
     return render(request,'Company/add-agent.html',{})
 
 
-def user_profile(request):
+def show_profile(request):
     admin=Admin.objects.get(id=request.user.id)
     usermodel=admin.user
     context = {
-        'admin':admin
+        'admin':admin ,
+        'usermodel':usermodel
     }
 
+    return render(request,'Company/show_profile.html',context)
+
+def edit_profile(request):
+    admin=Admin.objects.get(id=request.user.id)
+    usermodel=admin.user
+    context = {
+        'admin':admin ,
+       }
     if request.method == 'POST':
-        if request.POST.get("change"):
-            oldpassword=request.POST['oldpassword']
-            newpassword=request.POST['newpassword']
-            usermodel.set_password(newpassword)
-            usermodel.save()
-
-        if request.POST.get("save"):
-            admin.about=request.POST['about']
-            admin.phone1=request.POST['phone']
-            admin.Company=request.POST['company']
-            admin.Country=request.POST['country']
-            admin.Job=request.POST['job']
-            admin.address=request.POST['address']
-            admin.facebook=request.POST['facebook']
-            admin.telegram=request.POST['telegram']
-            admin.instagram=request.POST['instagram']
-
-            usermodel.first_name=request.POST['first_name']
+        admin.about=request.POST['about']
+        admin.phone1=request.POST['phone']
+        admin.Company=request.POST['company']
+        admin.Country=request.POST['country']
+        admin.Job=request.POST['job']
+        admin.address=request.POST['address']
+        admin.facebook=request.POST['facebook']
+        admin.telegram=request.POST['telegram']
+        admin.instagram=request.POST['instagram']
+        usermodel.first_name=request.POST['first_name']
         if len(request.FILES['img']) != 0:
             admin.profile_pic.delete()
             admin.profile_pic=request.FILES['img']
         admin.save()
         usermodel.save()
-        return redirect ('users-profile')
+        return redirect ('show_profile')
+    return render(request,'Company/edit_profile.html',context)
 
-    return render(request,'Company/users-profile.html',context)
+
+
+def change_password(request):
+    admin=Admin.objects.get(id=request.user.id)
+    u = User.objects.get(username__exact=request.user.username)
+    
+    
+    context = {
+        'admin':admin ,
+       }
+    if request.method == 'POST':
+        if request.POST.get("change"):
+            oldpassword=request.POST['oldpassword']
+            newpassword=request.POST['newpassword']
+            u.set_password(newpassword)
+            u.save()
+
+    return render(request,'Company/chage_password.html',context)
+
+
+def change_password_m(request):
+    user=User.objects.get(id=request.user.id)
+  
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            usermodel = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'Company/chage_password.html', {
+        'form': form
+    })
+
+def change_profile_pic(request):
+    admin=Admin.objects.get(id=request.user.id)
+    usermodel=admin.user
+    context = {
+        'admin':admin ,
+        'usermodel':usermodel
+    }
+    if request.method == 'POST':
+        if len(request.FILES['img']) != 0:
+            admin.profile_pic.delete()
+            admin.profile_pic=request.FILES['img']
+            admin.save()
+            return redirect ('edit_profile')
+        else:
+            return render(request,'Company/change_profile_pic.html',context)
+    return render(request,'Company/change_profile_pic.html',context)
+
+def delete_profile_pic(request):
+    admin=Admin.objects.get(id=request.user.id)
+    usermodel=admin.user
+    context = {
+        'admin':admin ,
+        'usermodel':usermodel
+    }
+    if len(admin.profile_pic) != 0:
+        admin.profile_pic.delete()
+        return redirect ('edit_profile')
+    
+    return render(request,'Company/edit_profile.html',context)
+
 
 def add_staff(request):
     return render(request,'Company/add-staff.html',{})
