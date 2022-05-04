8657bf4f8e67318c489059b44245b910f547999a
Efrem Yohanis <102489707+efremjon@users.noreply.github.com>
2022-05-04 14:34:48 -0700

    agent-dashbord updated

--- Agent/views.py
+++ Agent/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def Agent_dashboard(request):
-    return render(request,'Agent/agent_dashbord.html',{})
+    return render(request,'Agent/agent-dashboard.html',{})
+
+def customer_order(request):
+     return render(request,'Agent/view-cust-orders.html',{})
+
     
