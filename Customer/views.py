5a37058f2f44e47adb960f8dab00419c1e89c627
Efrem Yohanis <102489707+efremjon@users.noreply.github.com>
2022-05-09 13:20:19 -0700

    customer page updated

--- Customer/views.py
+++ Customer/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 
 
 def Customer_dashboard(request):
-    return render(request,'Customer/Customer-base.html')
+    return render(request,'Customer/home.html')
     
 def make_order(request):
-    return render(request,'Customer/make-order.html')
+    return render(request,'Customer/a.html')
 
 def send_delivery(request):
     return render(request,'Customer/send-delivery-status.html')
