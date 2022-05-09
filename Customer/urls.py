a4b94bbaae8f0615e1fe63a8edee5f8fde26d24a
Efrem Yohanis <102489707+efremjon@users.noreply.github.com>
2022-05-09 09:11:33 -0700

    customer page

--- Customer/urls.py
+++ Customer/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('',views.Customer_dashboard,name="Customer_dashbord"),
+    path('make_order/',views.make_order,name="make_order"),
+    path('send_delivery/',views.send_delivery,name="send_delivery"),
+    path('transaction_history/',views.transaction_history,name="transaction_history"),
     
     
     
--- Customer/views.py
+++ Customer/views.py
@@ -4,5 +4,13 @@ from django.shortcuts import render
 
 
 def Customer_dashboard(request):
-    return render(request,'Customer/Customer_dashbord.html',{})
+    return render(request,'Customer/Customer-base.html')
     
+def make_order(request):
+    return render(request,'Customer/make-order.html')
+
+def send_delivery(request):
+    return render(request,'Customer/send-delivery-status.html')
+
+def transaction_history(request):
+    return render(request,'Customer/transaction_history.html')
